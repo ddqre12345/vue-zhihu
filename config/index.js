72757a4af02dc008a0cd499efc7608cd0a85be20
hutingji <hutingji@zhijin.com>
2017-04-05 13:50:39 +0800

    优化

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
-var path = require( 'path' )
+const path = require( 'path' );
 
 module.exports = {
 	build: {
@@ -40,4 +40,4 @@ module.exports = {
 		// just be aware of this issue when enabling this option.
 		cssSourceMap: false
 	}
-}
+};
